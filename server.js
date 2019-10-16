601fae85ef975aa339beab0aa8336b1a07eda74f
kiyimba1 <edwineddie54@gmail.com>
2019-10-16 22:14:52 +0300

    added chat logic

--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-
-var app = express();
-
-app.use(express.static(__dirname));
-
-var server = app.listen(3000, () => {
-    console.log('server is running on port', server.address().port);
-});
