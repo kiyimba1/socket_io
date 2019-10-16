5e069a99e1279accefcc506252c8d12f0d812a6f
kiyimba1 <edwineddie54@gmail.com>
2019-10-16 21:47:51 +0300

    added static files

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express();
 
+app.use(express.static(__dirname));
+
 var server = app.listen(3000, () => {
     console.log('server is running on port', server.address().port);
 });
